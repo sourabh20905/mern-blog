604f09f36241fab48f4331e36355630430e6120e
sourabh20905 <sourabh20905@gmail.com>
2024-01-30 00:17:54 +0530

    create test controller and routes

--- api/index.js
+++ api/index.js
@@ -2,10 +2,13 @@ import express from "express";
 const app = express();
 import mongoose from "mongoose"
 import dotenv from "dotenv";
+import Userrouter from "./routes/user.route.js";
 dotenv.config();
 mongoose.connect(process.env.MONGO)
 .then(()=>console.log("db connected"))
 .catch((e)=>console.log(e));
+
+app.use("/api/v1",Userrouter);
 app.listen(3000,(req,res)=>{
   console.log("server started...")
 })
